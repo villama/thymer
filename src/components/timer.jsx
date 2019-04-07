8d9103bf47ee832d3e282bfb38a4a62a59c0c8e7
Julius Villamayor <juliusvillamayor@gmail.com>
2019-04-07 15:12:06 -0700

    Start and stop with the button

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -4,35 +4,44 @@ class Timer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      count: 100
+      count: 100,
+      buttonLabel: "Start"
     }
   }
 
   render() {
-    const { count } = this.state
+    const { count, buttonLabel } = this.state
     return (
       <div>
         <h1>Current count: {count}</h1>
-        <button onClick={this.startTimer}>Start</button>
+        <button onClick={this.timerButtonClicked}>{buttonLabel}</button>
       </div>
     )
   }
 
-  startTimer = () => {
-    setInterval(() => {
-      this.setState(prevState => ({
-        count: prevState.count - 1
-      }))
-    }, 1000)
+  timerButtonClicked = () => {
+    if (this.state.buttonLabel === "Start") {
+      this.setState({
+        intervalID: setInterval(() => {
+          this.setState(prevState => ({
+            count: prevState.count - 1
+          }))
+        }, 1000)
+      })
+      this.setState({
+        buttonLabel: "Stop"
+      })
+    } else {
+      clearInterval(this.state.intervalID)
+      this.setState({
+        buttonLabel: "Start"
+      })
+    }
   }
 
-  componentDidMount() {
-    // this.startTimer = setInterval(() => {
-    //   this.setState(prevState => ({
-    //     count: prevState.count - 1
-    //   }))
-    // }, 1000)
-  }
+  componentDidMount() {}
+
+  componentWillUnmount() {}
 }
 
 export default Timer
