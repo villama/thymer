ada58db4e61027210d111744fc9f5d3577a7009d
Julius Villamayor <juliusvillamayor@gmail.com>
2019-04-30 16:10:13 -0700

    graphic responds to start/stop

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
-import Graphic from "./graphic"
+import image_mp4 from "../assets/villager.mp4"
+import image_ogg from "../assets/villager.ogg"
 
 class Timer extends Component {
   constructor(props) {
@@ -17,11 +18,17 @@ class Timer extends Component {
   render() {
     return (
       <div className="Timer">
-        <Graphic percent={this.state.timeLeft / this.state.startTime} />
+        <video id="graphic" className="Graphic" muted>
+          <source src={image_mp4} type="video/mp4" />
+          <source src={image_ogg} type="video/ogg" />
+          Your browser does not support the video tag.
+        </video>
+
         <h2>
           timeLeft:
           <div name="timeLeft">{(this.state.timeLeft / 1000).toFixed(1)}</div>
         </h2>
+
         <button onClick={this.timerButtonClicked} name="timer-button">
           {this.state.buttonLabel}
         </button>
@@ -29,6 +36,7 @@ class Timer extends Component {
     )
   }
 
+  // Handles button clicks
   timerButtonClicked = () => {
     if (this.timerState === 0) {
       // Start the timer
@@ -38,6 +46,7 @@ class Timer extends Component {
       this.setState({
         buttonLabel: "Stop"
       })
+      this.vid.play()
     } else {
       // Pause the timer
       clearInterval(this.updateThreadID)
@@ -46,9 +55,11 @@ class Timer extends Component {
       this.setState({
         buttonLabel: "Start"
       })
+      this.vid.pause()
     }
   }
 
+  // This is called every 100 ms when the button is clicked
   updateTimeLeft = () => {
     const currentTime = Date.now()
     const elapsed = this.lastUpdate == null ? 0 : currentTime - this.lastUpdate
@@ -56,6 +67,13 @@ class Timer extends Component {
     this.setState({
       timeLeft: this.state.timeLeft - elapsed
     })
+  }
+
+  componentDidMount() {
+    this.vid = document.getElementById("graphic")
+    console.log(this.vid)
+    this.vid.playbackRate = 0.5
+    this.vid.oncontextmenu = false
   }
 }
 
